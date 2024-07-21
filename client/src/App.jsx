c7d26047653116c5e1332c14c297d94714d3af68
OrlinGichev <o.gichev.genik@gmail.com>
2024-07-21 11:43:29 +0300

    install react-router-dom
    add path for home component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/header/header"
 import Home from "./components/home/home"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
  
@@ -7,8 +8,10 @@ function App() {
   return (
     <div id="box">
       <Header/>
-      
-      <Home />
+          <Routes>
+              <Route path="/" element={<Home />} />     
+
+          </Routes>
     </div>
   )
 }
