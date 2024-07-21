abaebafc2fe3f0c91e5fa5a5391bcb204f48fccd
OrlinGichev <o.gichev.genik@gmail.com>
2024-07-21 13:55:33 +0300

    show dynamic new game

--- client/src/components/game-list/GameList.jsx
+++ client/src/components/game-list/GameList.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import * as gameService from "../../services/gameServices";
+import GameListItem from "./game-list-itme/GameListItem";
 
 
 export default function GameList () {
@@ -17,35 +18,15 @@ export default function GameList () {
         <section id="catalog-page">
             <h1>All Games</h1>
             {/* <!-- Display div: with inhtmlFormation about every game (if any) --> */}
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg"/>
-                    <h6>Action</h6>
-                    <h2>Cover Fire</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg"/>
-                    <h6>Action</h6>
-                    <h2>Zombie lang</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg"/>
-                    <h6>Action</h6>
-                    <h2>MineCraft</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-            </div>
+            {games.map(game => (
+                <GameListItem key={game._id} {...game}/>
+            ))}
 
             {/* <!-- Display paragraph: If there is no games  --> */}
-            <h3 className="no-articles">No articles yet</h3>
+
+            { games.length === 0 && (
+                <h3 className="no-articles">No articles yet</h3>
+            )}            
         </section>
     )
 }
