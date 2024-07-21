97c458957152d4879c98a6e368646edf45c2f850
OrlinGichev <o.gichev.genik@gmail.com>
2024-07-21 12:10:17 +0300

    add Login component and path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
+import { Routes, Route } from "react-router-dom"
 import Header from "./components/header/header"
 import Home from "./components/home/home"
-import { Routes, Route } from "react-router-dom"
+import GameList from "./components/game-list/GameList"
+import Login from "./components/login/Login"
 
 function App() {
  
 
   return (
     <div id="box">
-      <Header/>
+      <Header />
           <Routes>
               <Route path="/" element={<Home />} />     
-
+              <Route path="/games" element={<GameList />} /> 
+              <Route path="/login" element={<Login />} />
           </Routes>
     </div>
   )
