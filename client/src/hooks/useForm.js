7b14bc97bb6bab75b74d488fc8fe07892aa939d7
OrlinGichev <o.gichev.genik@gmail.com>
2024-07-25 21:39:12 +0300

    Create useForm.js

---
+++ client/src/hooks/useForm.js
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+export default function useForm (SubmitHandler,initialValues) {
+
+    const [values, setValues] = useState(initialValues); 
+
+    const onChange = (e) => {
+        setValues(state => ({
+            ...state,
+            [e.target.name] : e.target.value
+        }))
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        SubmitHandler(values)
+    }
+
+    return {
+        values,
+        onChange,
+        onSubmit
+    }
+
+}
