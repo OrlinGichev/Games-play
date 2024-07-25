1fa3a3fe9d4cc715ad06fe921258f3c5287a136c
OrlinGichev <o.gichev.genik@gmail.com>
2024-07-25 21:39:58 +0300

    Update Login.jsx

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,24 +1,46 @@
 import { Link } from "react-router-dom";
 
+import useForm from "../../hooks/useForm";
+
 export default function Login() {
+    const { value, onChange, onSubmit } = useForm({
+        email: '',
+        password: '',
+    }
+        
+    );
+
     return (
         <section id="login-page" className="auth">
-        <htmlForm id="login">
+        <form id="login" onSubmit={onSubmit}>
 
             <div className="container">
                 <div className="brand-logo"></div>
                 <h1>Login</h1>
                 <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
+                <input 
+                    type="email" 
+                    id="email" 
+                    name="email" 
+                    placeholder="Sokka@gmail.com"
+                    onChange={onChange}
+                    value={value.email} 
+                />
 
                 <label htmlFor="login-pass">Password:</label>
-                <input type="password" id="login-password" name="password" />
+                <input 
+                    type="password" 
+                    id="login-password" 
+                    name="password" 
+                    onChange={onChange}
+                    value={value.password}  
+                />
                 <input type="submit" className="btn submit" value="Login" />
                 <p className="field">
                     <span>If you do not have profile click <Link to="/register">here</Link></span>
                 </p>
             </div>
-        </htmlForm>
+        </form>
     </section>
     )
 }
