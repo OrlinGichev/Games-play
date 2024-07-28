24e551908e3c9ab0830cd2e7e7599d01c754b913
OrlinGichev <o.gichev.genik@gmail.com>
2024-07-28 13:15:47 +0300

    replace logic from App to AuthContext

--- client/src/contexts/authContext.jsx
+++ client/src/contexts/authContext.jsx
@@ -1,5 +1,8 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
+import * as authService from "../services/authService";
+import Path from "../paths";
 
 const AuthContext = createContext();
 
@@ -7,10 +10,54 @@ AuthContext.displayName = "AuthContext";
 
 export const AuthProvider = ({
     children,
-    value,
 }) => {
+
+    const [auth, setAuth] = useState(() => {
+        localStorage.removeItem('accessToken');
+        return {};
+      });
+      const navigate = useNavigate();
+    
+    
+      const loginSubmitHandler = async (values) =>{
+    
+        const result = await authService.login(values.email, values.password);
+    
+        setAuth(result);
+    
+        localStorage.setItem('accessToken', result.accessToken);
+    
+        navigate(Path.Home);
+      };
+    
+      const registerSubmitHandler = async (values) =>{
+        const result = await authService.register(values.email, values.password);
+    
+        setAuth(result);
+    
+        localStorage.setItem('accessToken', result.accessToken);
+    
+        navigate(Path.Home);
+    
+      }
+    
+      const logoutHandler = () => {
+        setAuth({});
+        localStorage.removeItem('accessToken');
+    
+      };
+    
+      const values = {
+        loginSubmitHandler,
+        registerSubmitHandler,
+        logoutHandler,
+        username: auth.username || auth.email,
+        email: auth.email,
+        isAuthenticated: !!auth.email,
+      }
+    
     return (
-        <AuthContext.Provider value={value}>
+        <AuthContext.Provider value={values}>
             {children}
         </AuthContext.Provider>
     );
