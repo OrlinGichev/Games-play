089227e948514f106116bd8dbc2982b8699f4cb8
OrlinGichev <o.gichev.genik@gmail.com>
2024-07-28 13:00:41 +0300

    Create Auth Provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 import * as authService from "./services/authService";
-import AuthContext from "./contexts/authContext";
+import { AuthProvider } from "./contexts/authContext";
 import Path from "./paths";
 
 import Header from "./components/header/header";
@@ -13,6 +13,7 @@ import Register from "./components/register/Register";
 import GameCreate from "./components/game-create/GameCreate";
 import GamesDetails from "./components/game-details/GameDetails";
 import Logout from "./components/logout/Logout";
+
 
 function App() {
   const [auth, setAuth] = useState(() => {
@@ -64,7 +65,7 @@ function App() {
   }
 
   return (
-    <AuthContext.Provider value={values}>
+    <AuthProvider value={values}>
         <div id="box">
           <Header />
               <Routes>
@@ -77,7 +78,7 @@ function App() {
                   <Route path={Path.Logout} element={<Logout />} />
               </Routes>
         </div>
-    </AuthContext.Provider>
+    </AuthProvider>
   )
 }
 
